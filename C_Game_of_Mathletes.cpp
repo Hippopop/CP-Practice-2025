17d61916d632ee31fcf47f3de48451bdaf1ef6a9
Mostafijul Islam <mostafijul929@gmail.com>
2025-02-12 15:32:43 +0600

    chore: maintenance commit! (03:32 PM - 12 February, 25)

---
+++ C_Game_of_Mathletes.cpp
@@ -0,0 +1,32 @@
+/*
+--- ---
+time: 15:31:32+06:00
+date: Wed 12, Feb 2025
+author: Mostafijul Islam
+problem: https://codeforces.com/problemset/problem/2060/C
+
+*INPUT*
+- ???
+
+*QUESTION*
+- ???
+--- ---
+*/
+
+#include <iostream>
+
+using namespace std;
+
+void solution() {
+  
+}
+
+int main() {
+  int t;
+  std::cin >> t;
+  while (t--) {
+    solution();
+    cout << endl;
+  }
+  return 0;
+}
