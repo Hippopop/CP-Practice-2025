c6f9f50efe928675c12436aa6d3149984c1b9f1a
Mostafijul Islam <mostafijul929@gmail.com>
2025-04-30 10:35:14 +0600

    chore: maintenance commit! (10:35 AM - 30 April, 25)

---
+++ C_Bewitching_Stargazer.cpp
@@ -0,0 +1,35 @@
+/*
+--- ---
+time: 10:26:43+06:00
+date: Wed 30, Apr 2025
+author: Mostafijul Islam
+email: mostafijul929@gmail.com
+github: https://github.com/Hippopop
+
+problem: https://codeforces.com/problemset/problem/2053/C
+
+*INPUT*
+- ???
+
+*QUESTION*
+- ???
+--- ---
+*/
+
+#include <iostream>
+
+#define li long long int
+using namespace std;
+
+static const string endW = "\n";
+void solution() {}
+
+int main() {
+  ios::sync_with_stdio(0);
+  cin.tie(0);
+
+  solution();
+  cout << endW;
+
+  return 0;
+}
